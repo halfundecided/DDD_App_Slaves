75e191ea1b01361d343b1d7a84b90eb1d6205cf5
Nathaniel Burgdorfer <n.burgdorfer@aol.com>
2018-04-02 23:58:28 -0400

    Modified ViewController.swift

--- IntercomApp/IntercomApp/ViewController.swift
+++ IntercomApp/IntercomApp/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+	
+var voiceRecording: AVAudioPlayer!
+var recordingSession: AVAudioSession!
+var micRecorder: AVAudioRecorder!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Record your message."
+	recordingSession = AVAudioSession.sharedInstance()
+
+	do {
+		try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+		try recordingSession.setActive(true)
+	}
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +34,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
