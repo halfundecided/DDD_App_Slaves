2d4cd2075147ad16eeb13b97014915fea174d22f
Jared Bass <jbass@host-b430-89.dhcp.stevens-tech.edu>
2018-04-05 20:11:29 -0400

    Added structure to record an m4a file. Records it to I assume a sandbox directory for each app.

--- IntercomApp/IntercomApp/ViewController.swift
+++ IntercomApp/IntercomApp/ViewController.swift
@@ -9,22 +9,101 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
-	
-var voiceRecording: AVAudioPlayer!
-var recordingSession: AVAudioSession!
-var micRecorder: AVAudioRecorder!
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var voiceRecording: AVAudioPlayer!
+    var recordingSession: AVAudioSession!
+    var micRecorder: AVAudioRecorder!
 
-
-    override func viewDidLoad() {
+    @IBOutlet weak var recordImage: UIImageView!
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        print(paths[0])
+        return paths[0]
+    }
+    
+    func startRecording()
+    {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        do {
+            micRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            micRecorder.delegate = self
+            micRecorder.record()
+            
+            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            stopRecording(success: false)
+        }
+    }
+    
+    func stopRecording(success: Bool)
+    {
+        micRecorder.stop()
+        micRecorder = nil
+        
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            // recording failed :(
+        }
+    }
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBAction func recordTapped(_ sender: Any)
+    {
+        if micRecorder == nil
+        {
+            startRecording()
+        }
+        else
+        {
+            stopRecording(success: true)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool)
+    {
+        if !flag {
+            stopRecording(success: false)
+        }
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        title = "Record your message."
-	recordingSession = AVAudioSession.sharedInstance()
+        recordingSession = AVAudioSession.sharedInstance()
 
-	do {
-		try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
-		try recordingSession.setActive(true)
-	}
+        do
+        {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission({[unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed
+                    {
+                        self.recordButton.isEnabled = true
+                    }
+                    else
+                    {
+                        self.recordButton.isEnabled = false
+                    }
+                }
+            })
+        }
+        catch
+        {
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
